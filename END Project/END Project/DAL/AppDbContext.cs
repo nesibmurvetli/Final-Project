efd136f290d0eb9aa90e058862241665ac3f8402
NesibMurvetli <nesib.murvetli986@gmail.com>
2023-04-12 11:44:19 +0400

    Sln file attachment.From to Back.SQL server connection

---
+++ END Project/END Project/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace END_Project.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- END Project/END Project/Program.cs
+++ END Project/END Project/Program.cs
@@ -1,7 +1,17 @@
+using END_Project.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+
+});
 
 var app = builder.Build();
 
